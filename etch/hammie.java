463a4afa6ba2dfdc12277fc062d79d84406d56c0
Alex Zhang <az177@eden.rutgers.edu>
2013-03-22 01:46:25 -0400

    uploaded files
    please excuse the file names.... naming classes was never my forte.

---
+++ etch/hammie.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class hammie{
+
+	public static void main(String[] args){
+
+		cutie frame = new cutie();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(440,440);
+		frame.setVisible(true);
+
+	}//end of main method
+}//end of hammie class
